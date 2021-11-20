dd26abde68cd36846bedc32851fdd2b173bdbdb9
lhenslee <lhenslee@vols.utk.edu>
2021-11-20 03:20:32 -0800

    Made a terrible logo haha, I tried

--- crypto_bot/build_bots.bat
+++ crypto_bot/build_bots.bat
@@ -2,7 +2,9 @@
 Taskkill /IM "BitcoinBot.exe" /F
 Taskkill /IM "BinanceBot.exe" /F
 Taskkill /IM "HeroBot.exe" /F
+Taskkill /IM "HunchoBot.exe" /F
 pyinstaller -y -F -w -i .\ico\bitcoin.ico -n BitcoinBot bitcoin_bot.py 
 pyinstaller -y -F -w -i .\ico\hero.ico -n HeroBot hero_bot.py 
 pyinstaller -y -F -w -i .\ico\bnb.ico -n BinanceBot bnb.py 
+pyinstaller -y -F -w -i .\ico\huncho.ico -n HunchoBot huncho_bot.py 
 DEL *.spec
