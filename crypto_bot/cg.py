f16e2c1dce89bf780fec134a7037add26368abd6
lhenslee <lhenslee@vols.utk.edu>
2021-11-24 11:23:23 -0800

    Implement new coin api

--- crypto_bot/cg.py
+++
@@ -1,4 +0,0 @@
-from coingecko.coin import Coin
-
-hero = Coin('HERO', 'metahero')
-print(hero.get_price())
--- crypto_bot/price_bot.py
+++ crypto_bot/price_bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 import discord
-from coingecko import CoinGecko
+from coingecko.coin import Coin
 from logger import make_logger
 
 
@@ -10,7 +10,7 @@ class PriceBot(discord.Client):
     def __init__(self, name, token_id, bot_token=None, frequency=10, **kwargs):
         super(PriceBot, self).__init__(**kwargs)
         self.logger = make_logger(name)
-        self.cg = CoinGecko(name, token_id)
+        self.coin = Coin(name, token_id)
         self.name = name
         self.frequency = frequency
         self.price = 0
@@ -21,33 +21,34 @@ class PriceBot(discord.Client):
         """Update bot nickname"""
         for guild in self.guilds:
             for member in guild.members:
-                self.logger.info(f'Updating the price to {self.price} in {guild}.')
+                self.logger.info(
+                    f'Updating the price to {self.price} in {guild}.')
                 await member.edit(nick=f'{self.name} ${self.price}')
                 self.logger.info(f'Successfully updated the price in {guild}.')
 
     async def update_price(self):
         """Update the price of the bot"""
         try:
-            price = self.cg.get_price()
+            price = self.coin.get_price()
         except json.decoder.JSONDecodeError:
-            self.logger.warning('Coingecko failed to get price while updating.')
+            self.logger.warning(
+                'Coingecko failed to get price while updating.')
             await asyncio.sleep(60)
             return
         if price != self.price:
             self.price = price
             await self.update_nickname()
-    
+
     async def continue_price_update(self):
         """Make sure the price updates at the frequency specified."""
         while True:
             await self.update_price()
             await asyncio.sleep(self.frequency)
-        
+
     async def on_ready(self):
         self.ready = True
         await self.continue_price_update()
 
     async def on_message(self, message):
         if message.author == self.user:
-            return 
-
+            return
