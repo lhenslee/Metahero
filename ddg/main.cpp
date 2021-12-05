52f9cbe098303953aef4b43e7787875e9224d81f
justin <justinlisenko@yahoo.com>
2021-12-04 16:22:07 -0800

    justin

---
+++ ddg/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "----   ----" << endl;
+    cout << "| 0  | |  0 | " << endl;
+    cout << " ---   ----" << endl;
+
+
+
+
+ 
+
+
+    return 0;
+}
